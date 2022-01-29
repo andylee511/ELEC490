dfef493d989396cfa4c4dec005fb1d23298c9d7a
Yen-Ju Lee <andylee222@gmail.com>
2022-01-28 23:35:09 -0500

    modified motion sensor node

--- ESP_IDF/espnow/main/espnow_example_main.c
+++ ESP_IDF/espnow/main/espnow_example_main.c
@@ -49,123 +49,51 @@ static uint16_t s_example_espnow_seq[EXAMPLE_ESPNOW_DATA_MAX] = { 0, 0 };
 #define WIFI_CONNECTED_BIT BIT0
 #define WIFI_FAIL_BIT BIT1
 static void example_espnow_deinit(example_espnow_send_param_t *send_param);
-static EventGroupHandle_t s_wifi_event_group;
-void NVS_Init(void)
-{
-    esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
-    {
-        ESP_ERROR_CHECK(nvs_flash_erase());
-        ret = nvs_flash_init();
-    }
-    ESP_ERROR_CHECK(ret);
-    ESP_LOGI("esp_err_t类型返回值：", " %d\n", ret);
-}
-// 系统事件循环处理函数 
-static void event_handler(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data)
-{
-    static int s_retry_num = 0;
-    // wifi事件状态为 start 时连接
-    if (event_base == WIFI_EVENT )
-    {   
-        if (event_id == WIFI_EVENT_STA_START)       // 事件id为STA开始
-        {
-            ESP_LOGI("WIFI_EVENT事件 ", "连接wifi\n");
-            esp_wifi_connect();            
-        }
-        else if (event_id == WIFI_EVENT_STA_DISCONNECTED)   // 事件id为STA断开，重新连接直到最大重连数
-        {
-            if (s_retry_num < EXAMPLE_ESP_MAXIMUM_RETRY)
-            {
-                esp_wifi_connect();
-                s_retry_num++;
-                ESP_LOGI("WIFI_EVENT事件：", "重试连接AP");
-            }
-            else
-            {
-                xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
-            }
-            ESP_LOGI("WIFI_EVENT事件：", "连接AP失败");
-        }
 
-    }
-    else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP)     // 系统事件为ip地址事件，且事件id为成功获取ip地址
-    {
-        ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
-        ESP_LOGI("IP_EVENT事件 ", "得到IP:" IPSTR, IP2STR(&event->ip_info.ip));
-        s_retry_num = 0;
-        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
-    }
-}
+#define DEFAULT_SCAN_LIST_SIZE 10
 
-void wifi_init_sta(void)
-{
-    
-    s_wifi_event_group = xEventGroupCreate();                   // 创建事件标志组，返回值为事件标志组句柄,根据WIFI的连接状况来做出相应动作。
+static const char *TAG1 = "scan";
 
-    ESP_ERROR_CHECK(esp_netif_init());                          // 初始化底层TCP/IP堆栈
-    ESP_ERROR_CHECK(esp_event_loop_create_default());           // 创建默认事件循环
-    esp_netif_create_default_wifi_sta();                        // 创建一个默认的WIFI-STA网络接口
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();        // 默认的wifi配置参数
-    ESP_ERROR_CHECK(esp_wifi_init(&cfg));                       // 根据 cfg 初始化 wifi
-    ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));    // 配置存储类型设置为 RAM
 
-    // 将事件处理程序注册到系统默认事件循环，分别是WiFi事件和IP地址事件
-    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL));
-    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL));
-
-    // 配置WiFi连接的 ssid password 参数
-    wifi_config_t wifi_config = {
-        .sta = {
-            .ssid = EXAMPLE_ESP_WIFI_SSID,
-            .password = EXAMPLE_ESP_WIFI_PASS,
-            .scan_method = WIFI_ALL_CHANNEL_SCAN,
-            .sort_method = WIFI_CONNECT_AP_BY_SIGNAL,
-            .threshold.rssi = -127,
-            .threshold.authmode = WIFI_AUTH_WPA2_PSK,
-            .pmf_cfg = {
-                .capable = true,
-                .required = false},
-        },
-    };
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_APSTA));                              // 工作模式设置为 STA
-    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));            // 根据 wifi_config 进行设置
-    ESP_ERROR_CHECK(esp_wifi_start());                                              // 启动wifi连接
-
-    /* 等待标志组。条件到达最大重试次数 WIFI_FAIL_BIT ，直到连接建立 WIFI_CONNECTED_BIT 或连接失败。位由event_handler()设置(参见上面) */
-    EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,                      // 需要等待的事件标志组的句柄 
-                                           WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,      // 需要等待的事件位
-                                           pdFALSE,                                 // 为pdFALSE时，在退出此函数之前所设置的这些事件位不变，为pdFALSE则清零
-                                           pdFALSE,                                 // 为pdFALSE时，设置的这些事件位任意一个置1就会返回，为pdFALSE则需全为1才返回
-                                           portMAX_DELAY);                          // 设置为最长阻塞等待时间，单位为时钟节拍
-
-    /* xEventGroupWaitBits()返回调用的位，根据事件标志组等待函数的返回值获取WiFi连接状态 */
-    if (bits & WIFI_CONNECTED_BIT)                                                  // WiFi连接成功事件
-    {
-        ESP_LOGI("标志bits状态：", "连接成功\n 名称:%s\n 密码:%s\n",EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);                     
-    }
-    else if (bits & WIFI_FAIL_BIT)
-    {
-        ESP_LOGI("标志bits状态：", "连接失败\n 名称:%s\n 密码:%s\n",EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
-    }
-    else
-    {
-        ESP_LOGE("标志bits状态：", "突发事件");
-    }
-
-    // 注销事件
-    ESP_ERROR_CHECK(esp_event_handler_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler));
-    ESP_ERROR_CHECK(esp_event_handler_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler));
-    vEventGroupDelete(s_wifi_event_group);                                            // 删除WiFi连接事件标志组
-}
+static uint8_t channel;
 
 /* WiFi should start before using ESPNOW */
-static void example_wifi_init(void)
+static void wifi_scan(void)
 {
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_t *sta_netif = esp_netif_create_default_wifi_sta();
+    assert(sta_netif);
+
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    uint16_t number = DEFAULT_SCAN_LIST_SIZE;
+    wifi_ap_record_t ap_info[DEFAULT_SCAN_LIST_SIZE];
+    uint16_t ap_count = 0;
+    memset(ap_info, 0, sizeof(ap_info));
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_start());
+    esp_wifi_scan_start(NULL, true);
+    ESP_ERROR_CHECK(esp_wifi_scan_get_ap_records(&number, ap_info));
+    ESP_ERROR_CHECK(esp_wifi_scan_get_ap_num(&ap_count));
+    ESP_LOGI(TAG1, "Total APs scanned = %u", ap_count);
+    for (int i = 0; (i < DEFAULT_SCAN_LIST_SIZE) && (i < ap_count); i++) {
+        ESP_LOGI(TAG1, "SSID \t\t%s", ap_info[i].ssid);
+        ESP_LOGI(TAG1, "RSSI \t\t%d", ap_info[i].rssi);
+        ESP_LOGI(TAG1, "Channel \t\t%d\n", ap_info[i].primary);
+        const char *temp = EXAMPLE_ESP_WIFI_SSID;
+        if (!strcmp((char *)ap_info[i].ssid, temp)) {
+            channel = ap_info[i].primary;
+        }
+        
+    }
+    printf("Channel = %d\n", channel);
+
+    ESP_ERROR_CHECK( esp_wifi_set_promiscuous(true));
+    ESP_ERROR_CHECK( esp_wifi_set_channel(channel, WIFI_SECOND_CHAN_NONE));
+    ESP_ERROR_CHECK( esp_wifi_set_promiscuous(false));
     ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
     ESP_ERROR_CHECK( esp_wifi_set_mode(ESPNOW_WIFI_MODE) );
     ESP_ERROR_CHECK( esp_wifi_start());
@@ -173,7 +101,10 @@ static void example_wifi_init(void)
 #if CONFIG_ESPNOW_ENABLE_LONG_RANGE
     ESP_ERROR_CHECK( esp_wifi_set_protocol(ESPNOW_WIFI_IF, WIFI_PROTOCOL_11B|WIFI_PROTOCOL_11G|WIFI_PROTOCOL_11N|WIFI_PROTOCOL_LR) );
 #endif
+
 }
+
+
 
 /* ESPNOW sending or receiving callback function is called in WiFi task.
  * Users should not do lengthy operations from this task. Instead, post
@@ -492,10 +423,12 @@ void app_main(void)
         ret = nvs_flash_init();
     }
     ESP_ERROR_CHECK( ret );
-
+    
+    wifi_scan();
     //example_wifi_init();
-    NVS_Init();
-    wifi_init_sta();
+    
+    //NVS_Init();
+   // wifi_init_sta();
     example_espnow_init();
 
 }

bedbff24e288b37cec7f9f069c362a41a3e81e60
Dinka211 <claire_aman@163.com>
2022-02-09 00:36:40 -0500

    new -app

--- Android/Capstone_app/app/src/main/java/com/example/capstone_app/HomeActivity.java
+++ Android/Capstone_app/app/src/main/java/com/example/capstone_app/HomeActivity.java
@@ -2,13 +2,125 @@ package com.example.capstone_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class HomeActivity extends AppCompatActivity {
+    Button temp,humidity,motion,airquality;
+    TextView temp_value,humidity_value,motion_value,airquality_value;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        getJSON("https://www.xu3u04bj6capstone.com/get-sensor-data.php");
+        temp=findViewById(R.id.temp); //temperature button
+        humidity=findViewById(R.id.humidity);// humidity button
+        motion=findViewById(R.id.motion); //motion button
+        airquality=findViewById(R.id.gas); //air quality  button
+
+        //Creating TEXTVIEW that recive data from SQL
+        temp_value=findViewById(R.id.temp_text); //sign in button
+        humidity_value=findViewById(R.id.humidity_text);
+        motion_value=findViewById(R.id.motion_text);
+        airquality_value=findViewById(R.id.air_quality_text);
     }
+
+    //this method is actually fetching the json string
+    private void getJSON(final String urlWebService) {
+
+
+        class GetJSON extends AsyncTask<Void, Void, String> {
+
+            //this method will be called before execution
+            //you can display a progress bar or something
+            //so that user can understand that he should wait
+            //as network operation may take some time
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+            }
+
+            //this method will be called after execution
+            //so here we are displaying a toast with the json string
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+                try {
+                    loadIntoTextView(s);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            //in this method we are fetching the json string
+            @Override
+            protected String doInBackground(Void... voids) {
+
+
+
+                try {
+                    //creating a URL
+                    URL url = new URL(urlWebService);
+
+                    //Opening the URL using HttpURLConnection
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+                    //StringBuilder object to read the string from the service
+                    StringBuilder sb = new StringBuilder();
+
+                    //We will use a buffered reader to read the string from service
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+                    //A simple string to read values from each line
+                    String json;
+
+                    //reading until we don't find null
+                    while ((json = bufferedReader.readLine()) != null) {
+
+                        //appending it to string builder
+                        sb.append(json + "\n");
+                    }
+
+                    //finally returning the read string
+                    return sb.toString().trim();
+                } catch (Exception e) {
+                    return null;
+                }
+
+            }
+        }
+
+        //creating asynctask object and executing it
+        GetJSON getJSON = new GetJSON();
+        getJSON.execute();
+    }
+    private void loadIntoTextView(String json) throws JSONException {
+        JSONArray jsonArray = new JSONArray(json);
+        String[] heroes = new String[jsonArray.length()];
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject obj = jsonArray.getJSONObject(i);
+          //  heroes[i] = obj.getString("name");
+            temp_value.setText(obj.getString("temperature"));
+            humidity_value.setText(obj.getString("humidity"));
+
+
+        }
+
+    }
+
+
 }
